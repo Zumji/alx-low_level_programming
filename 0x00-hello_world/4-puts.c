a56c2344035998dd68b054e25f3295f21e59cf9e
Zumji <ambert24real@gmail.com>
2022-12-08 23:25:23 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
  * main - Eniry point
  *
- * return: Always 0 (success)
+ * return:Always 0 (success)
 */
 
 int main(void)
 
 {
 	puts("\"programming is lIke building a multilingual puzzle");
-	
-	return(0);
+	return (0);
 }

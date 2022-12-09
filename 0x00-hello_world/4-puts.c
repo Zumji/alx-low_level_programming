61e5771b0f6fc2c7526bc632990b4e08d70f164b
Zumji <ambert24real@gmail.com>
2022-12-08 19:24:45 -0800

    pu

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
+
 /**
  * main -entry point
  *
  * return: Always 0 (success)
- */
+*/
 
 int main(void)
 {
-	puts(\""programming is lIke building a multilingual puzzle);
+	puts(\"programming is lIke building a multilingual puzzle);
 	
 	return 0;
 

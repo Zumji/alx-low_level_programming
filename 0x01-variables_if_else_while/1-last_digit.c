2be72788022d87b116302ef2f14dab1c0b3b2619
Zumji <ambert24real@gmail.com>
2022-12-10 05:40:51 -0800

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ n = rand() - RAND_MAX / 2;
 digit = n % 10;
 if (n > 0)
 {
-printf("Last digit of %d is %d and greater than 5\n", n, digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 }
 else if ((n < 6) && (n < 0))
 {
-printf("Last of %d is %d and less than 6 and not 0\n", n, digit);
+printf("Last of %d is %d and is less than 6 and not 0\n", n, digit);
 }
 else if (n == 0)
 {
-printf("Last digit of %d is %d and 0 \n", n, digit);
+printf("Last digit of %d is %d and is 0 \n", n, digit);
 }
 return (0);
 }

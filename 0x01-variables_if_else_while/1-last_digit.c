d870f9f8d344c21ebba7b2f8b92f4c8bea1e2fe3
Zumji <ambert24real@gmail.com>
2022-12-10 04:41:29 -0800

    my second readme file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+/**
+ * main - main function to generate a random number
+ *
+ *  Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	int digit = n % 10;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 5)
+{
+printf("%d and is greater than 5\n", digit);
+}
+else if ((n < 6) && (n < 0))
+{
+printf("%d and is less than 6 and not 0\n", digit);
+}
+else if (n == 0)
+{
+printf("%d and is 0 \n", digit);
+}
+return (0);
+}

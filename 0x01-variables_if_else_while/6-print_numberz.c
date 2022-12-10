c384c1bab7c2a3663e53d4c6ec7a5f5b6e786e94
Zumji <ambert24real@gmail.com>
2022-12-10 12:43:03 -0800

    6 prints

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+
+while (n <= 9)
+	{
+putchar(n);
+n++;
+	}
+putchar('\n');
+return (0);
+}

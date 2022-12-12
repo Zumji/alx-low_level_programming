28a882cd9b53ef33c1706363a1eed1da1ae7222b
Zumji <ambert24real@gmail.com>
2022-12-12 04:36:11 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,17 @@
 int main(void)
 {
 int n;
-for (n = 48; n <= 57; n++)
+for (n = 48; n <= 58; n++)
 {
-	putchar(",", n);
-	putchar(' ');
-}/**
+	putchar(n);
+	
+}
 		if (n != 58)
+
 {
-			putchar(' ');
-}*/
+	putchar(',');
+	putchar(' ');
+}
 putchar('\n');
 return (0);
 }

637935105c8f96b83e8594af3123c3ceab114df9
Zumji <ambert24real@gmail.com>
2022-12-14 09:37:14 -0800

    alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,11 @@
  */
 void print_alphabet_x10(void)
 {
+int num = 0;
 char c = 'a';
-int num = '0';
-
-for (num = '0'; num <= '9'; num++))
+for (num = 0; num <= 9; num++))
 {
-	for (c = 'a'; c <= 'z'; c++)
-	{
+for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 _putchar('\n');
 }

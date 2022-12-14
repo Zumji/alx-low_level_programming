2a16354bf793f4544fedd94398b4ff898ca449aa
Zumji <ambert24real@gmail.com>
2022-12-14 09:02:30 -0800

    alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,11 @@ void print_alphabet_x10(void)
 {
 char c = 'a';
 int num = '0';
-		
+
 for (c = 'a'; c <= 'z'; c++)
-	{
 for (num = '0'; num <= '9'; num++)
+{
 _putchar(c);
+}
 _putchar('\n');
-	}
 }

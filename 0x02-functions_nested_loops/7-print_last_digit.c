63104019c811e77b63dff4c21e8f1b4f8f47ec7e
Zumji <ambert24real@gmail.com>
2022-12-16 00:32:25 -0800

    last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 	int last;
 
 	last = (n % 10);
-	if (last >= 0)
+	if (last < 0)
 	{
-		_putchar(last);
+		last += -1;
 	}
-	else
+	_putchar(last + 0);
 	{
 
 	return (last);

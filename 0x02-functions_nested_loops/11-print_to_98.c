e027df87b3f5bbf899e6e628f3164eb015ac40dd
Zumji <ambert24real@gmail.com>
2022-12-16 05:18:04 -0800

    print 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_to_98 - it is use to print numbers <= 98 and numbers
+ *  >= 98
+ *@n: it is the int in the question
+ *Return: return the printed numbers
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+	while (n > 98)
+	printf("%d, ", n--);
+	printf("%d\n", n);
+	}
+	else
+	{
+	while (n < 98)
+	printf("%d, ", n++);
+	printf("%d\n", n);
+	}
+}
+

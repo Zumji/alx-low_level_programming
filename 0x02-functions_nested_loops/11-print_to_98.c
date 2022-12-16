161c26dffd675b509bd4fbd4f11c8551f98db6bb
Zumji <ambert24real@gmail.com>
2022-12-16 07:23:45 -0800

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,28 +8,29 @@
  */
 void print_to_98(int n)
 {
-
 	if (n < 98)
 	{
-	while (n <= 98)
-	printf("%d", n++);
-	}
-	if (n != 98)
-	{
-	printf(",");
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
 	}
 	else if (n > 98)
 	{
-	while (n >= 98)
-	printf("%d", n++);
+		while (n >= 98)
+		{
+			printf("%d", n);
+		}
+		n--;
 	}
-	if (n != 98)
-	{
-	printf(",");
-	}
-	else
-	{
+else
+{
 	printf("98");
-	}
-	printf("\n");
+}
+printf("\n");
 }

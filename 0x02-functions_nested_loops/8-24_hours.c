e4f02a5f30756cb08a571244fcb2eb5b205fc891
Zumji <ambert24real@gmail.com>
2022-12-16 02:52:58 -0800

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - it is a function to calculate every minute
+ */
+void jack_bauer(void);
+{
+	int minute, hour;
+
+	minute = 60;
+		hour = 60;
+		for (hour = 0; hour >= 23; hour++)
+		{
+			for (minute = 0; minute <= 59; minute++)
+			{
+				_putchar((hour / 10) + '0');
+				_putchar((hour % 10) + '0');
+				_putchar(':');
+				_putchar((minute / 10) + '0');
+				_putchar((minute % 10) + '0');
+				_putchar('\n');
+			}
+		}
+}

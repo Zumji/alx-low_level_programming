9e085da400e50c6cfd9fdebe06ccd19eae0d8812
Zumji <ambert24real@gmail.com>
2022-12-19 12:54:56 -0800

    strlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen - this function prints out the length of a string
+ * @s: is the address of the string
+ * Return: the value of the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (str [1])
+	{
+		_putchar(str[1]);
+		i++;
+	}
+	_putchar('\n');
+}

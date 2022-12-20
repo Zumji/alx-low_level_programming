f54b1c858039c88f4b1cea48c3902e4b0a600d5c
Zumji <ambert24real@gmail.com>
2022-12-20 05:12:09 -0800

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *str)
 	int i;
 	int j = 0;
 
-	while (str[j] !='\0')
+	while (str[j] != '\0')
 	{
 		j++;
 	}
-	for ( i = 0; i < j; i += 2)
+	for (i = 0; i < j; i += 2)
 	{
 		putchar(str[i]);
 	}

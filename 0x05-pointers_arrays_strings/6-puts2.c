ffa6822b9cdc83161b04de8f8a20d64cb415a678
Zumji <ambert24real@gmail.com>
2022-12-20 05:04:18 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - print every other character of a strong
+ * @str: the string to be treated
+ * return: this return void
+ */
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] !='\0')
+	{
+		j++;
+	}
+	for ( i = 0; i < j; += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
+

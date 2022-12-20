bfbe11b74b87cc1b7f890a85bc4be9e1f7798545
Zumji <ambert24real@gmail.com>
2022-12-20 02:03:26 -0800

    my first string len

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _strlen - this function prints out the length of a string
+ * _strlen - this function prints out the length of a string
  * @s: is the address of the string
  * Return: the value of the length of the string
  */
-int main (void)
+int _strlen(char *s)
 {
 	char *str;
 	int len;

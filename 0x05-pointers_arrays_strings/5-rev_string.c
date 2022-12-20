42434213623f06100370060768b99ae930b201b8
Zumji <ambert24real@gmail.com>
2022-12-20 01:46:50 -0800

    5 rev-string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - revises a string
+ * @s: this is a string that is to be revised
+ * Return: it is to return void
+ */
+void rev_string(char *s)
+{
+int len = 0;
+int index = 0;
+char tmp;
+
+while (s[index++])
+len++;
+
+for (index = len - 1; index >= len / 2; index--)
+{
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
+}
+}
+

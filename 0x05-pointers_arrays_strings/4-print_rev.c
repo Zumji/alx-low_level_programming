e742914f790127729b7d2f4df5556de01486278a
Zumji <ambert24real@gmail.com>
2022-12-20 01:02:13 -0800

    revise order of string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - it prints string in the revise order.
+ * @s: it is the string to be printer
+ * Return: it is to return the string
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+	while (s[c] != '\0')
+		{
+			c++
+		}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}

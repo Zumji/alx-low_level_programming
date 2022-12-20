53a3cc4cde2c17f564b6340b8f264b6e3db3d611
Zumji <ambert24real@gmail.com>
2022-12-20 09:22:15 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - this function prints a string
+ * @str: This is the string character in the function
+ * Return: return the string
+ */
+
+void _puts(char *str)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them"
+	putchar('str');
+	return (0);
+	}
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 int len = 0;
 int index = 0;
 char tmp;
-
+                                                                                                                              
 while (s[index++])
 len++;
 

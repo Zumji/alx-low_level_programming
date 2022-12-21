7b98f49ae956d18c2f6a47e764e2c9396dbfc3e3
Zumji <ambert24real@gmail.com>
2022-12-21 09:27:56 -0800

    print

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,19 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		putchar('\n');
-	}
-	else
-	{
-		int i;
-		
-		for (i = 1; i <= n; i++)
-		{
-			putchar('_');
-		}
-		putchar('\n');
-	}
+
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
+int i;
+
+for (i = 1; i <= n; i++)
+{
+putchar('_');
+}
+putchar('\n');
+}
 }

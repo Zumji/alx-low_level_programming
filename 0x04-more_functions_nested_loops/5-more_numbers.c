fd0cefa9d18d46873ef66e48574a1baa586474f1
Zumji <ambert24real@gmail.com>
2022-12-21 09:21:54 -0800

    print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,16 @@
  */
 void more_numbers(void)
 {
-	int i, j;
-	
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 15; j++)
-		{
-			if (j >= 10)
-				putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-		}
-		putchar('\n');
-	}
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+if (j >= 10)
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+}
+putchar('\n');
+}
 }

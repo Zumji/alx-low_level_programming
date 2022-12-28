b2786967ce2937ff9472fd0afc41496dfff96aae
Zumji <ambert24real@gmail.com>
2022-12-28 03:17:56 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,15 +17,15 @@ for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-_putchar (' ');
+_putchar(' ');
 }
-_putchar (92);
-_putchar ('\n');
+_putchar(92);
+_putchar('\n');
 }
 }
 else
 }
-_putchar ('\n');
+_putchar('\n');
 }
 }
 

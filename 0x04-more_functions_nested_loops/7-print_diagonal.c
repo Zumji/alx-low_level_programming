4e30045fbbfb8b556494ff1f89313908f0baf30c
Zumji <ambert24real@gmail.com>
2022-12-28 02:52:15 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,28 +8,22 @@
 
 void print_diagonal(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
+
 int i, j;
 
+if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
-if (j < i)
-{
-_putchar('\\');
-}
-else if (j < i)
-{
 _putchar(' ');
 }
+else
+{
 _putchar('\n');
 }
 }
 }
 }
+

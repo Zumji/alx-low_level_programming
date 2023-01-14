b1353c0f754758d63b4136f3a5bc0bf1969b662e
Zumji <ambert24real@gmail.com>
2023-01-14 00:01:50 -0800

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,13 +4,13 @@
 /**
  * main - prints the number of arguments passed in the program
  * @argc: number of argments
- * @argv: array of strings
+ * @argv: array of arguments
  * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /* ignore argv*/ 
+	(void) argv; /* ignore argv*/
 	printf("%d\n", argc - 1);
 	return (0);
 }

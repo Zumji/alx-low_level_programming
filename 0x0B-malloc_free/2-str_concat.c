f3d5363f8c18561d852fcd33acfc396add13a98c
Zumji <ambert24real@gmail.com>
2023-01-17 00:15:28 -0800

    stringcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,23 +14,15 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j = 0, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	len1 = 0;
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 	len2 = 0;
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 	size = len1 + len2;
 	nstr = malloc((sizeof(char) * size) + 1);
 	/* this is to check if malloc is successful*/

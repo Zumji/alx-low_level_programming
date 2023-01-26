755dab766c7f13f8250e765e4940e33b503dbb6d
Zumji <ambert24real@gmail.com>
2023-01-25 17:09:24 -0800

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ for (index = 0; index < n; index++)
 {
 printf("%d", va_arg(nums, int));
 
-if (i != (n - 1) && separator != NULL)
+if (index != (n - 1) && separator != NULL)
 printf("%s", separator);
 
 }

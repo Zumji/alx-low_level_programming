a0342aae933b928d8b53e276494783fee230ffe3
Zumji <ambert24real@gmail.com>
2023-02-02 00:50:49 -0800

    list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 	
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nill)\n");
+			printf("[%d] %s\n", 0, "(nill)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		nodes++;
 		h = h->next;
+		nodes++;
 	}
 	return (nodes);
 }

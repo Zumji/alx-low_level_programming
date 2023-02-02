cd1a1b79f605efb65d2bd7f5bd43addfda673300
Zumji <ambert24real@gmail.com>
2023-02-02 11:06:04 -0800

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: Apointer to thev head of the list_t list
+ * @h: Apointer to the head of the list_t list
  * Return: the number of nodes in the list_t list
  */
 size_t print_listint(const listint_t *h)

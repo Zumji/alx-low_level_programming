a536d2704810c02864b6f975e0f0c4e10ba762b6
Zumji <ambert24real@gmail.com>
2023-02-02 01:33:07 -0800

    list add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <string.h>
+
 
 /**
  * add_node - adds a new mode at the beggining of a list_t list
@@ -22,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-	for (len = 0; strc[len];)
+	for (len = 0; str[len];)
 		len++;
 	new->str = dup;
 	new->len = len;

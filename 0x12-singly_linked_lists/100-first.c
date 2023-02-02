4e42ca8eac54f32be2bfc34fd3af37666e4579a0
Zumji <ambert24real@gmail.com>
2023-02-02 03:22:22 -0800

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * hare - prints a string before the main function in execution
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}

3edac0678a7d2c77a32a0989104af1f74108a3b6
Zumji <ambert24real@gmail.com>
2023-02-03 00:55:22 -0800

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a listint_t list
+ * @head: A pointer to the head of the list_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}

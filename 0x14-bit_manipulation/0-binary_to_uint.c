4c458eca469b35634fe433dfecdb6b36ee4dcbe2
Zumji <ambert24real@gmail.com>
2023-02-13 05:58:36 -0800

    binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,16 @@
 unsigned int binary_to_unit(const char *b)
 {
 	int i;
-		unsigned int ambert = 0;
+		unsigned int dec_val= 0;
 
-	if (lb)
+	if (!b)
 		return (0);
 	for (i = 0; b[i]; i++)
 
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		ambert = 2 * ambert + (b[i] -'0');
+		dec_val = 2 * dec_val + (b[i] -'0');
 	}
-	return (ambert);
+	return (dec_val);
 }

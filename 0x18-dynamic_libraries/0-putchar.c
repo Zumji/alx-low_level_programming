9d3959ae0e052ba495d7c634b82015bae8a99bef
Zumji <ambert24real@gmail.com>
2023-04-08 02:45:42 -0700

    script

--- 0x18-dynamic_libraries/0-putchar.c
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-char _putchar(char c) 
+int _putchar(char c) 
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,6 @@ char _putchar(char c)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
